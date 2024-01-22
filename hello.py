f1384444253d5c355130e27fce3fec213a367085
Purity Jepkorir <jepkorirpurity8@gmail.com>
2024-01-22 19:59:23 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,54 @@
+#data types i.e Strings,Integers,Floats,List,Tuple,Set,Dictionary,None,Booleans
+
+print("Hello")
+
+#declare variables 
+age = 20  #integer
+price = 19.95 #float
+first_name ="Purity" #string
+is_online = False  #boolean
+print(age)
+
+#Receiving user input 
+#name = input("What is your name? ")
+#print("My name is" + name)
+
+
+#Type Conversion
+#birth_year = input("Enter your birth year:   ")
+#age = 2020 - int(birth_year) 
+
+#x += 2
+
+#Logical Operators i.e AND,OR,NOT
+
+temperature = 35  
+
+if temperature > 30:
+    print("Its a hot day")
+elif temperature == 20:
+    print("Its not hot")
+else:
+    print("Not conducive") 
+
+
+# While is used to repeat a block of code multiple times
+    
+i= 0
+
+while i <= 5:
+    print(i)
+    i += 1
+
+
+# Lists
+names = ["John","Mark","Luke","Michael"]
+print(names[0])
+print(names[0:3])  #display from index 0 to 2
+# 1 in names
+# List Methods i.e List.append(6)- last, List.insert(8)- beginning, List.remove(3),List.length
+
+# For loop
+numbers = [1,2,3,4,5]
+for item in numbers:
+    print(item) 
